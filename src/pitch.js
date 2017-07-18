e4ba40e351ff65c9ee40af986bfb512012b24160
Christopher Swenson <christopher@looker.com>
2017-07-17 20:43:14 -0700

    First source commit, adding Interval, Pitch, and Accidental classes, and some methods and constants for each one.

---
+++ src/pitch.js
@@ -0,0 +1,260 @@
+'use strict';
+
+function addErrorTypes(classConstructor, className, errorNames) {
+  errorNames.forEach(function(errorName) {
+    classConstructor[errorName] = function(message) {
+      this.name = className + '.' + errorName;
+      this.message = message || "";
+    }
+    classConstructor[errorName].prototype = Error.prototype;
+  });
+}
+
+function positiveModulus(a, n) {
+  return ((a % n) + n) % n;
+}
+
+function intervalUpDown(interval, pitch, below) {
+  var multiplier = below ? -1 : 1;
+  var number = pitch.scalarIndex;
+  var octaveOffset = Math.abs(Math.floor((number + multiplier * (interval.size - 1)) / 7));
+
+  var name = Pitch.Name.all[(number + multiplier * (interval.size - 1)) % 7];
+  var accidental = Accidental.fromOffset(pitch.accidental.offset + multiplier * interval.quality.getOffset(interval.size));
+  var octave = new Pitch.Octave(pitch.octave.number + multiplier * octaveOffset);
+  return new Pitch(name, accidental, octave);
+}
+
+class Interval {
+  constructor(quality, size) {
+    this.size = size;
+    this.quality = quality;
+    if (size === null || size === undefined) {
+      throw new Interval.NullSizeError("Interval Size may not be null.");
+    } else if (quality === null || quality === undefined) {
+      throw new Interval.NullQualityError("Interval Quality may not be null.")
+    } else if (!quality.isAllowed(size)) {
+      throw new Interval.InvalidQualityError("Invalid Quality for Interval with Size " + size + ".");
+    }
+    this.halfSteps = quality.getHalfSteps(size);
+  }
+
+  above(pitch) {
+    return intervalUpDown(this, pitch, false);
+  }
+
+  below(pitch) {
+    return intervalUpDown(this, pitch, true);
+  }
+
+  isEqualTo(other) {
+    return this.size == other.size
+      && this.quality.isEqualTo(other.quality);
+  }
+
+  isEnharmonicTo(other) {
+    return this.halfSteps == other.halfSteps;
+  }
+}
+
+addErrorTypes(Interval, "Interval", [
+  "NullSizeError",
+  "NullQualityError",
+  "InvalidQualityError",
+]);
+
+Interval.Quality = class {
+  constructor(isAllowed, getOffset) {
+    this.getOffset = getOffset;
+    this.isAllowed = isAllowed;
+  }
+
+  getHalfSteps(size) {
+    return [11, 0, 2, 4, 5, 7, 9][size % 7] + Math.floor(size / 7) * 12 + this.getOffset(size);
+  }
+
+  isEqualTo(other) {
+    return this === other;
+  }
+}
+
+var isAllowed2367 = function(size) {
+  return ~[0, 2, 3, 6].indexOf(size % 7);
+}
+
+var isAllowed1458 = function(size) {
+  return ~[1, 4, 5].indexOf(size % 7);
+}
+
+var isAllowedAll = function(size) {
+  return true;
+}
+
+Interval.Quality.Major      = new Interval.Quality(isAllowed2367, (size) => 0);
+Interval.Quality.Perfect    = new Interval.Quality(isAllowed1458, (size) => 0);
+Interval.Quality.Minor      = new Interval.Quality(isAllowed2367, (size) => -1);
+Interval.Quality.Augmented  = new Interval.Quality(isAllowedAll,  (size) => 1);
+Interval.Quality.Diminished = new Interval.Quality(isAllowedAll,  function(size) {
+  return ~[1, 4, 5].indexOf(size % 7) ? -1 : -2;
+});
+
+Interval.Quality.fromOffset = function(size, offset) {
+  if (isAllowed2367(size % 7)) {
+    switch(positiveModulus(offset, 12)) {
+      case  1: return Interval.Quality.Augmented;
+      case  0: return Interval.Quality.Major;
+      case -1: return Interval.Quality.Minor;
+      case -2: return Interval.Quality.Diminished;
+    }
+  } else {
+    switch(positiveModulus(offset, 12)) {
+      case  1: return Interval.Quality.Augmented;
+      case  0: return Interval.Quality.Perfect;
+      case -1: return Interval.Quality.Diminished;
+    }
+  }
+  throw new Interval.Quality.InvalidOffsetError('Invalid offset ' + offset + ' (' + positiveModulus(offset, 12) + ') for Interval of size ' + size + '.');
+}
+
+addErrorTypes(Interval.Quality, "Interval.Quality", [
+  "InvalidOffsetError",
+]);
+
+Interval.Size = {
+  Unison: 1,
+  Second: 2,
+  Third: 3,
+  Fourth: 4,
+  Fifth: 5,
+  Sixth: 6,
+  Seventh: 7,
+  Octave: 8,
+  Ninth: 9,
+  Tenth: 10,
+  Eleventh: 11,
+  Twelfth: 12
+};
+
+["Major", "Perfect", "Minor", "Augmented", "Diminished"].forEach(function(qualityName) {
+  var quality = Interval.Quality[qualityName];
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].forEach(function(size) {
+    if (quality.isAllowed(size)) {
+      Interval[qualityName + size] = new Interval(quality, size);
+    }
+  });
+});
+
+Interval.between = function(pitch1, pitch2) {
+  var scalarOffset = Math.abs(pitch1.scalarIndex - pitch2.scalarIndex) + 1;
+  var octaveOffset = Math.abs(pitch1.octave.number - pitch2.octave.number);
+  var totalSize = scalarOffset + 7 * octaveOffset;
+  var chromaticOffset = Math.abs(pitch1.chromaticIndex - pitch2.chromaticIndex);
+  var defaultOffset = Interval.Quality.Perfect.getHalfSteps(totalSize);
+  var quality = Interval.Quality.fromOffset(totalSize, chromaticOffset - defaultOffset);
+  return new Interval(quality, totalSize);
+}
+
+class Accidental {
+  constructor(offset) {
+    this.offset = offset;
+  }
+
+  isEqualTo(other) {
+    return this.offset == other.offset;
+  }
+}
+
+var offsetMap = {};
+offsetMap[ 0] = Accidental.Natural     = new Accidental( 0);
+offsetMap[ 1] = Accidental.Sharp       = new Accidental( 1);
+offsetMap[-1] = Accidental.Flat        = new Accidental(-1);
+offsetMap[ 2] = Accidental.DoubleSharp = new Accidental( 2);
+offsetMap[-2] = Accidental.DoubleFlat  = new Accidental(-2);
+
+Accidental.fromOffset = function(offset) {
+  return offsetMap[offset] || Accidental(offset);
+}
+
+class Pitch {
+  constructor(name, accidental, octave) {
+    this.name = name;
+    this.accidental = accidental;
+    this.octave = octave;
+  }
+
+  get chromaticIndex() {
+    return chromaticIndices[this.name.stringValue] + this.accidental.offset;
+  }
+
+  get scalarIndex() {
+    return Pitch.Name.all.indexOf(this.name);
+  }
+
+  isEqualTo(other) {
+    return this.name === other.name
+      && this.accidental.isEqualTo(other.accidental)
+      && this.octave.isEqualTo(other.octave);
+  }
+}
+
+var pitchNamesFixed = false;
+Pitch.Name = class {
+  constructor(stringValue) {
+    if (pitchNamesFixed) {
+      throw new Pitch.Name.IllegalConstructionError("Illegal construction of Pitch.Name with string value '" + stringValue + "'.");
+    }
+    this.stringValue = stringValue;
+  }
+}
+
+addErrorTypes(Pitch.Name, "Pitch.Name", [
+  "IllegalConstructionError",
+]);
+
+Pitch.Name = {
+  C: new Pitch.Name('C'),
+  D: new Pitch.Name('D'),
+  E: new Pitch.Name('E'),
+  F: new Pitch.Name('F'),
+  G: new Pitch.Name('G'),
+  A: new Pitch.Name('A'),
+  B: new Pitch.Name('B')
+}
+pitchNamesFixed = true;
+
+Pitch.Name.all = 'CDEFGAB'.split("").map((letter) => Pitch.Name[letter]);
+
+var chromaticIndices = {}
+chromaticIndices['C'] = 0;
+chromaticIndices['D'] = 2;
+chromaticIndices['E'] = 4;
+chromaticIndices['F'] = 5;
+chromaticIndices['G'] = 7;
+chromaticIndices['A'] = 9;
+chromaticIndices['B'] = 11;
+
+Pitch.Octave = class {
+  constructor(number) {
+    this.number = number;
+  }
+
+  isEqualTo(other) {
+    return this.number == other.number;
+  }
+}
+
+Pitch.Name.all.forEach(function(pitchName) {
+  ["Natural", "Sharp", "Flat", "DoubleSharp", "DoubleFlat"].forEach(function(accidentalName) {
+    var accidental = Accidental[accidentalName];
+    [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function(octaveNumber) {
+      return new Pitch.Octave(octaveNumber);
+    }).forEach(function(octave) {
+      var pitch = new Pitch(pitchName, accidental, octave);
+      Pitch[pitchName.stringValue + accidentalName + octave.number] = pitch;
+    });
+  });
+});
+
+module.exports.Pitch = Pitch;
+module.exports.Accidental = Accidental;
+module.exports.Interval = Interval;
---
+++ test/pitch.test.js
@@ -0,0 +1,163 @@
+var assert = require('assert');
+
+var Pitch = require('../src/pitch').Pitch;
+var Accidental = require('../src/pitch').Accidental;
+var Interval = require('../src/pitch').Interval;
+
+describe('Pitch', function() {
+
+  describe('Interval', function() {
+
+    it('should accept good quality-size pairings', function() {
+      [
+        Interval.Size.Second,
+        Interval.Size.Third,
+        Interval.Size.Sixth,
+        Interval.Size.Seventh,
+      ].forEach(function(size) {
+        [
+          Interval.Quality.Major,
+          Interval.Quality.Minor,
+          Interval.Quality.Diminished,
+          Interval.Quality.Augmented
+        ].forEach(function(quality) {
+          new Interval(quality, size);
+        });
+      });
+
+      [
+        Interval.Size.Unison,
+        Interval.Size.Fourth,
+        Interval.Size.Fifth,
+        Interval.Size.Octave,
+      ].forEach(function(size) {
+        [
+          Interval.Quality.Perfect,
+          Interval.Quality.Diminished,
+          Interval.Quality.Augmented
+        ].forEach(function(quality) {
+          new Interval(quality, size);
+        });
+      });
+    });
+
+    it('should reject bad quality-size pairings', function() {
+      [
+        Interval.Size.Second,
+        Interval.Size.Third,
+        Interval.Size.Sixth,
+        Interval.Size.Seventh,
+      ].forEach(function(size) {
+        assert.throws(function() {
+          new Interval(Interval.Quality.Perfect, size);
+        });
+      });
+
+      [
+        Interval.Size.Unison,
+        Interval.Size.Fourth,
+        Interval.Size.Fifth,
+        Interval.Size.Octave,
+      ].forEach(function(size) {
+        [
+          Interval.Quality.Major,
+          Interval.Quality.Minor
+        ].forEach(function(quality) {
+          assert.throws(function() {
+            new Interval(quality, size);
+          });
+        });
+      });
+    });
+
+    it('should reject null size', function() {
+      assert.throws(function() {
+        new Interval(Interval.Quality.Minor, null);
+      });
+    });
+
+    it('should reject null quality', function() {
+      assert.throws(function() {
+        new Interval(Interval.Quality.Minor, null);
+      });
+    });
+
+  });
+
+  describe('Interval.isEqualTo', function() {
+
+    it('should return true when intervals are the same object', function() {
+      var interval = new Interval(Interval.Quality.Major, Interval.Size.Sixth);
+      assert(interval.isEqualTo(interval));
+    });
+
+    it('should return true when intervals are the same', function() {
+      var interval1 = new Interval(Interval.Quality.Major, Interval.Size.Sixth);
+      var interval2 = new Interval(Interval.Quality.Major, Interval.Size.Sixth);
+      assert(interval2.isEqualTo(interval1));
+      assert(interval1.isEqualTo(interval2));
+    });
+
+    it('should return false when intervals are not the same', function() {
+      var interval1 = new Interval(Interval.Quality.Augmented, Interval.Size.Fourth);
+      var interval2 = new Interval(Interval.Quality.Diminished, Interval.Size.Fifth);
+      assert(!interval2.isEqualTo(interval1));
+      assert(!interval1.isEqualTo(interval2));
+    });
+
+  });
+
+  describe('Interval.isEnharmonicTo', function() {
+
+    it('should return true when intervals are the same object', function() {
+      var interval = new Interval(Interval.Quality.Major, Interval.Size.Sixth);
+      assert(interval.isEnharmonicTo(interval));
+    });
+
+    it('should return true when intervals are enharmonic', function() {
+      var interval1 = new Interval(Interval.Quality.Augmented, Interval.Size.Fourth);
+      var interval2 = new Interval(Interval.Quality.Diminished, Interval.Size.Fifth);
+      assert(interval2.isEnharmonicTo(interval1));
+      assert(interval1.isEnharmonicTo(interval2));
+    });
+
+    it('should return false when intervals are not enharmonic', function() {
+      var interval1 = new Interval(Interval.Quality.Augmented, Interval.Size.Fourth);
+      var interval2 = new Interval(Interval.Quality.Perfect, Interval.Size.Fifth);
+      assert(!interval2.isEnharmonicTo(interval1));
+      assert(!interval1.isEnharmonicTo(interval2));
+    });
+
+  });
+
+  describe('Interval.between', function() {
+
+    it('should be correct', function() {
+      assert(Interval.between(Pitch.CNatural4, Pitch.CNatural4).isEqualTo(Interval.Perfect1));
+      assert(Interval.between(Pitch.CNatural4, Pitch.CNatural5).isEqualTo(Interval.Perfect8));
+      assert(Interval.between(Pitch.CNatural4, Pitch.FSharp4).isEqualTo(Interval.Augmented4));
+    });
+
+  });
+
+  describe('Interval.above', function() {
+
+    it('should be correct', function() {
+      assert(Interval.Perfect1.above(Pitch.CNatural4).isEqualTo(Pitch.CNatural4));
+      assert(Interval.Augmented4.above(Pitch.CNatural4).isEqualTo(Pitch.FSharp4));
+      assert(Interval.Perfect8.above(Pitch.CNatural4).isEqualTo(Pitch.CNatural5));
+    });
+
+  });
+
+  describe('Interval.below', function() {
+
+    it('should be correct', function() {
+      assert(Interval.Perfect1.below(Pitch.CNatural4).isEqualTo(Pitch.CNatural4));
+      assert(Interval.Augmented4.below(Pitch.FSharp4).isEqualTo(Pitch.CNatural4));
+      assert(Interval.Perfect8.below(Pitch.CNatural5).isEqualTo(Pitch.CNatural4));
+    });
+
+  });
+
+});
